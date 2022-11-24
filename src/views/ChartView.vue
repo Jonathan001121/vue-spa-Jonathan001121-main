d04abac6799b79890a1c66b5d5b92c4d6a812841
Jonathan Chan <20202288@life.hkbu.edu.hk>
2022-11-25 00:01:59 +0800

    first commit

---
+++ src/views/ChartView.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="home">
+
+        <apexchart width="580" type="donut" :options="options" :series="series"></apexchart>
+        <button type="button" class="btn btn-primary" @click="logout">Log Out</button>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import { ref , onMounted} from "vue";
+
+
+export default {
+  name: 'ChartView',
+  components: {
+   
+  }, setup() {
+        const options = ref({});
+        const series = ref([44, 55, 41, 17, 15]);
+        const logout = function() {
+  localStorage.removeItem('user');
+  location.reload()
+}
+        onMounted(async () => {
+          let token = localStorage.getItem("user");
+
+var response = await fetch("/api/bookings/aggregate/groupby", {
+    headers: {
+      "x-access-token": token
+    }
+});
+});
+        return {
+            options, series,logout
+        }
+    },
+}
+</script>
